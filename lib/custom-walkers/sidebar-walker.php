0372a1009ed793856466654d6ad4116668f26128
Unknown <tdwells90@gmail.com>
2017-04-25 10:38:30 +0100

    Custom walker changes
    Displaying the pages at the right depth now.
    - Dropdowns not working.
    - Needs Overview page at top of section.

--- lib/custom-walkers/sidebar-walker.php
+++ lib/custom-walkers/sidebar-walker.php
@@ -1,36 +1,40 @@
 <?php
 
-class Sidebar_Walker extends Walker_Nav_Menu {
+class Sidebar_Walker extends Walker_Page {
     
 	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 
-		$class_names = $value = '';
-		$classes = '';
-		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
 
-		if ( $args->has_children ) {
-			$class_names .= 'dropdown';
-		}
-		
-		if ( $item->current == true ) {
-			$class_names .= ' active';
-		}
-
-		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
 		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
 		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
 
-		$output .= $indent . '<li' . $id . $value . $class_names .'>';
+		if( $args->has_children ) {
+			$output .= $indent . '<li class="dropdown" ' . $id . '>';
+		} else {
+			$output .= $indent . '<li' . $id . '>';
+		}
 
-		$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-        $attributes .= ! empty( $item->url )        ? ' href="'   . '#' .'"' : '';
+		$atts = array();
+		$atts['title']  = ! empty( $item->title )	? $item->title	: '';
+		$atts['target'] = ! empty( $item->target )	? $item->target	: '';
+		$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
+		$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+		
+		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( ! empty( $value ) ) {
+				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
 
 		$item_output = $args->before;
 
 		$item_output .= '<a href="' . get_permalink( $item->ID ) . '" '. $attributes .'>';
 		$item_output .= $args->link_before;
+		$item_output .= apply_filters( 'the_title', $item->post_title, $item->ID );
 		$item_output .= $args->link_after;
 		$item_output .= '</a>';
 
