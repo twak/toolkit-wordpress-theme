823232358478d4c6e3d6f496db9132e689a3202b
peteredwards <p.l.edwards@leeds.ac.uk>
2017-04-26 11:10:04 +0100

    refactored ACF code in lib/acf and started to refactor other stuff in lib

---
+++ lib/admin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Toolkit theme admin
+ * stores theme version, upgrade routines, etc.
+ */
+
+if ( ! class_exists( 'tk_admin' ) ) {
+	class tk_admin
+	{
+        /**
+         * theme version
+         */
+        public static $version = "0.2.0";
+
+        public static function register()
+        {
+
+        	
+        }
+
+	}
+}
