85640bb1c5581455b759a6ebc014742393ffb0d0
Tom Wells <tdwells90@gmail.com>
2017-06-30 15:12:26 +0100

    Fixed header sizing

--- header.php
+++ header.php
@@ -169,7 +169,7 @@ $GLOBALS['colour'] = tk_colour();
 			    <div class="wrapper-pd-xs <?php if(!$GLOBALS[ 'full_width' ]){ echo "wrapper-lg"; }?>">
 			        <div class="local-header-inner">
 			            <div class="local-header-title">
-			                <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+			                <a href="<?php bloginfo('url'); ?>"><?php bloginfo( 'name' ); ?></a>
 			            </div>
 			            <div class="local-header-search">
 			                <button class="icon-font sm-toggle-search btn-icon js-site-search-toggle" data-toggle="collapse" data-target="#sitesearch">
