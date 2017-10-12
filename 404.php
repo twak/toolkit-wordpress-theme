1af9928f1363d17c608fb8a0d07c40abb1f87a81
peteredwards <p.l.edwards@leeds.ac.uk>
2017-10-12 16:09:51 +0100

    Added configurable text and title for 404 page to theme options

--- 404.php
+++ 404.php
@@ -1,8 +1,18 @@
-<?php get_header(); ?>
+<?php get_header(); 
+$title = get_field('tk_404_page_title', 'option');
+if ( ! $title ) {
+    $title = 'Page not found';
+}
+$content = get_field('tk_404_page_content', 'option');
+if ( ! $content ) {
+    $content = 'Sorry, the page you are looking for could not be found on this site';
+}
+$content = apply_filters('the_content', $content);
+?>
 
 <div class="wrapper-xs wrapper-pd p-t p-b">			
-	<h1 class="heading-underline">Page not found</h1>	
-	<?php get_template_part('searchform'); ?>
+	<h1 class="heading-underline"><?php echo $title; ?></h1>	
+	<?php echo $content; ?>
 </div>
 
 <?php get_footer(); ?>
