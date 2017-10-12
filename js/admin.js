66c9bb30a3d74d99e24a9daeb15c3e70f753eefa
peteredwards <p.l.edwards@leeds.ac.uk>
2017-10-12 13:54:43 +0100

    Made posts types in news events and posts widget non-selectable when the plugins aren't activated. Corrected check for featured image inclusion.

--- js/admin.js
+++ js/admin.js
@@ -4,87 +4,105 @@
 
 // actions on Advanced Custom Fields
 (function($){
-	acf.add_action('ready', function(){
-		
-		function sharingCheckboxes()
-		{
-			// if none of the options is checked or disabled, check the hide option
-			if (!$('[data-name="tk_social_media_sharing_links"] input:disabled').length && !$('[data-name="tk_social_media_sharing_links"] input:checkbox[value!="none"]').filter(':checked').length) {
-				$('[data-name="tk_social_media_sharing_links"] input[value="none"]').prop('checked', true);
-			}
-			// if the hide checkbox is checked, disable all the others
-			if ( $('[data-name="tk_social_media_sharing_links"] input[value="none"]').prop('checked')) {
-				$('[data-name="tk_social_media_sharing_links"] input[value!="none"]').prop('disabled', true);
-			} else {
-				// if not, enable the others
-				$('[data-name="tk_social_media_sharing_links"] input[value!="none"]').prop('disabled', false);
-			}
-		}
-		$('[data-name="tk_social_media_sharing_links"] input:checkbox').on('click', function(){
-			sharingCheckboxes();
-		});
-		sharingCheckboxes();
-	});
+    // ensures news and events checkboxes in the News Events and Posts widget
+    // are disabled when the plugins aren't present
+	var newsEventsCheckboxes = function(){
+        if (tkadmin && tkadmin.news_plugin !== "1" ) {
+            $('[data-name="news_events_widget_options"] input[value="news"]').prop('disabled', true);
+        }
+        if (tkadmin && tkadmin.events_plugin !== "1" ) {
+            $('[data-name="news_events_widget_options"] input[value="events"]').prop('disabled', true);
+        }
+    },
+    sharingCheckboxes = function(){
+        // if none of the options is checked or disabled, check the hide option
+        if (!$('[data-name="tk_social_media_sharing_links"] input:disabled').length && !$('[data-name="tk_social_media_sharing_links"] input:checkbox[value!="none"]').filter(':checked').length) {
+            $('[data-name="tk_social_media_sharing_links"] input[value="none"]').prop('checked', true);
+        }
+        // if the hide checkbox is checked, disable all the others
+        if ( $('[data-name="tk_social_media_sharing_links"] input[value="none"]').prop('checked')) {
+            $('[data-name="tk_social_media_sharing_links"] input[value!="none"]').prop('disabled', true);
+        } else {
+            // if not, enable the others
+            $('[data-name="tk_social_media_sharing_links"] input[value!="none"]').prop('disabled', false);
+        }
+    };
 
-	// limit number of characters on title field
-	$(document).ready(function(){
+    $(document).ready(function(){
+        if (typeof acf !== 'undefined') {
+            // add selection logic to sharing checkboxes and widget checkboxes
+            acf.add_action('ready', function(){
+        		$('[data-name="tk_social_media_sharing_links"] input:checkbox').on('click', function(){
+        			sharingCheckboxes();
+        		});
+        		sharingCheckboxes();
+                newsEventsCheckboxes();
+        	});
 
-		var maxTitleLength = 75;
+            // make sure news and events checkboxes are disabled when widget is added to page
+            acf.add_action('append', function(){
+                newsEventsCheckboxes();
+            });
+        }	
+        // limit number of characters on title field in posts and pages
+        if ($('body').hasClass('post-type-post') || $('body').hasClass('post-type-page')) {
+    		var maxTitleLength = 75;
 
-		// Add span to the input displaying the characters remaining
-		$( '<span class="title-counter" style="position: absolute; right: 1px;top:1px; padding:.645em; width:1.5em; color: #777; background:#eee; text-rendering: optimizeLegibility;"></span>' ).insertAfter( '#title' );
+    		// Add span to the input displaying the characters remaining
+    		$( '<span class="title-counter" style="position: absolute; right: 1px;top:1px; padding:.645em; width:1.5em; color: #777; background:#eee; text-rendering: optimizeLegibility;"></span>' ).insertAfter( '#title' );
 
-        $('#titlewrap').after($('<div/>', {'id':'titleTooLongWarning','style':'color:#f33;border-left:4px solid #f33;background:#fff;padding:0 10px;'}).html('<p>Your title is <span class="title-counter"></span> characters long. Please consider using a shorter title.</p>'));
-		
-        // colours the title input red and advises user when the maximum number of characters is reached
-		function titleWarning(){
+            $('#titlewrap').after($('<div/>', {'id':'titleTooLongWarning','style':'color:#f33;border-left:4px solid #f33;background:#fff;padding:0 10px;'}).html('<p>Your title is <span class="title-counter"></span> characters long. Please consider using a shorter title.</p>'));
+    		
+            // colours the title input red and advises user when the maximum number of characters is reached
+    		function titleWarning(){
 
-            var getLength = $('#title').val().length;
+                var getLength = $('#title').val().length;
 
-			// Give the counter a value
-            var remaining = (getLength > maxTitleLength)? getLength: (maxTitleLength - getLength);
-			
-            $( '.title-counter' ).text( remaining );
+    			// Give the counter a value
+                var remaining = (getLength > maxTitleLength)? getLength: (maxTitleLength - getLength);
+    			
+                $( '.title-counter' ).text( remaining );
 
-			if ( getLength >= 0 ) {
-                
+    			if ( getLength >= 0 ) {
+                    
 
-    			// If length is maxTitleLength characters or more, add highlight
-    			if( getLength > maxTitleLength ) {
+        			// If length is maxTitleLength characters or more, add highlight
+        			if( getLength > maxTitleLength ) {
 
-                    $('#titleTooLongWarning').show();
+                        $('#titleTooLongWarning').show();
 
-    				// Add the colours
-    				$('#title').css({
-    					'border': '1px solid red',
-    					'boxShadow': '0 0 2px red'
-    				});
+        				// Add the colours
+        				$('#title').css({
+        					'border': '1px solid red',
+        					'boxShadow': '0 0 2px red'
+        				});
 
-    				$( '.title-counter' ).css({
-    					'color': 'red'
-    				});
+        				$( '.title-counter' ).css({
+        					'color': 'red'
+        				});
 
-    			// Otherwise remove inline styles
-    			} else {
-                    $('#titleTooLongWarning').hide();
+        			// Otherwise remove inline styles
+        			} else {
+                        $('#titleTooLongWarning').hide();
 
-    				$('#title').css({
-    					'border': '',
-    					'boxShadow': ''
-    				});
+        				$('#title').css({
+        					'border': '',
+        					'boxShadow': ''
+        				});
 
-    				$( '.title-counter' ).css({
-    					'color': 'inherit'
-    				});
-    			}
-            }
-		}
+        				$( '.title-counter' ).css({
+        					'color': 'inherit'
+        				});
+        			}
+                }
+    		}
 
-		// watch title field
-		$(document).on('keyup', '#title', titleWarning);
+    		// watch title field
+    		$(document).on('keyup', '#title', titleWarning);
 
-		// highlight title field
-		titleWarning();
+    		// highlight title field
+    		titleWarning();
+        }
 
         // only allow custom menu widgets in sidebar
         $('.widget').on( 'dragcreate dragstart', function( event, ui ){
